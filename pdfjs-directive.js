e24a93e22db0d9efc11e8bc33a6597c32828a2ff
--global <carmenbourlonlong@gmail.com>
2016-05-16 17:22:22 -0500

    new changes

--- pdfjs-directive.js
+++ pdfjs-directive.js
@@ -3,7 +3,17 @@ app.directive('pdfjs', function() {
   return {
     restrict: 'E',
     transclude: true,
-    scope: {},
+    scope: {
+      pdfPath: '=pdfPath'
+    },
+    link: function(scope, element, attrs) {
+      console.log('link func');
+      console.log('scope', scope);
+      console.log('attrs', attrs);
+      if(attrs.pdfpath) {
+        console.log(attrs.pdfpath);
+      }
+    },
     templateUrl: 'simpleviewer.html'
   };
 });
