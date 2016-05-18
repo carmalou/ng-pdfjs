3480089f87d33484485ea29ac4d5431a5e87a97e
--global <carmenbourlonlong@gmail.com>
2016-05-18 09:16:19 -0500

    cleaning this up a bit

--- simpleviewer.js
+++ simpleviewer.js
@@ -1,25 +1,14 @@
-/* Copyright 2014 Mozilla Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 'use strict';
 
 function simpleViewer(pdfpath) {
   testForPDFJS();
 
   var DEFAULT_URL = pdfpath;
+  var SEARCH_FOR = '';
+  var PAGE_TO_VIEW = 1;
+  var SCALE = 1.0;
 
+  // generate elements for viewer
   var parentViewer = document.createElement('div');
   parentViewer.setAttribute('id', 'viewerContainer');
   document.body.appendChild(parentViewer);
@@ -30,25 +19,14 @@ function simpleViewer(pdfpath) {
 
   parentViewer.appendChild(childViewer);
 
-  // The workerSrc property shall be specified.
-  //
   PDFJS.workerSrc = 'node_modules/pdfjs-dist/build/pdf.worker.js';
-
-  // Some PDFs need external cmaps.
-  //
-  // PDFJS.cMapUrl = '../../build/dist/cmaps/';
-  // PDFJS.cMapPacked = true;
-
-  // var DEFAULT_URL = './relativity.pdf';
-  var SEARCH_FOR = ''; // try 'Mozilla';
-  var PAGE_TO_VIEW = 1;
-  var SCALE = 1.0;
 
   var container = document.getElementById('viewerContainer');
 
   // (Optionally) enable hyperlinks within PDF files.
   var pdfLinkService = new PDFJS.PDFLinkService();
 
+  // generate and create viewer
   var pdfViewer = new PDFJS.PDFViewer({
     container: container,
     linkService: pdfLinkService,
@@ -73,10 +51,7 @@ function simpleViewer(pdfpath) {
   // Loading document.
   function getDocument() {
     PDFJS.getDocument(DEFAULT_URL).then(function (pdfDocument) {
-      // Document loaded, specifying document for the viewer and
-      // the (optional) linkService.
       pdfViewer.setDocument(pdfDocument);
-
       pdfLinkService.setDocument(pdfDocument, null);
     });
   }
